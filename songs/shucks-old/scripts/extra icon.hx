d50240b1adf0614bc6fd77db4eacb8654fc48d37
💜 Rodney, An Imaginative Furball 💙 <okmilkrb@gmail.com>
2025-11-09 02:27:00 -0500

    awwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww shucks

--- songs/shucks-old/scripts/extra icon.hx
+++ songs/shucks-old/scripts/extra icon.hx
@@ -1,30 +1,25 @@
 var isPlayer:Bool = true;
 var iconP3:HealthIcon;
 
-function postCreate(){    
+function postCreate(){
     iconP3 = new HealthIcon(gf != null && gf.icon!=null?gf.icon:gf.curCharacter, isPlayer);
     iconP3.cameras = [camHUD];
     add(iconP3);
     iconP3.alpha = 0;
     iconP3.setPosition(iconP1.x + 70, iconP2.y - (!downscroll ? 35 : -15));
     iconP3.scale.set(0.8, 0.8);
-
 }
 
 function update(elapsed:Float){
-
-    if (curStep < 2306){
+    if (curStep < 2306)
         iconP3.x = iconP1.x + 200;
-    }
-    if (curStep < 2816){
+    if (curStep < 2816)
         iconP3.health = isPlayer? iconP1.health : iconP2.health;
-    }
 }
 
 function beatHit(){
-        iconP3.scale.set(1, 1);
-        FlxTween.tween(iconP3.scale, {x: 0.8, y: 0.8}, (0.5 * (1 / (Conductor.bpm / 60))), {ease: FlxEase.circOut});
-
+    iconP3.scale.set(1, 1);
+    FlxTween.tween(iconP3.scale, {x: 0.8, y: 0.8}, (0.5 * (1 / (Conductor.bpm / 60))), {ease: FlxEase.circOut});
 }
 
 function stepHit(curStep) {
--- songs/shucks-old/scripts/songimage.hx
+++ songs/shucks-old/scripts/songimage.hx
@@ -8,22 +8,21 @@ playCutscenes = true;
 function onCountdown(e) {
     e.cancel();
 }
+
 function postCreate() {
-    image = new FlxSprite(-999, -50).loadGraphic(Paths.image('songimage/' + PlayState.SONG.meta.name));
+    /* image = new FlxSprite(-999, -50).loadGraphic(Paths.image('songimage/' + PlayState.SONG.meta.name));
     image.cameras = [camHUD];
     image.scale.x = 0.6;
     image.scale.y = 0.6;
-    insert(members.indexOf(iconP1), image);
-
+    insert(members.indexOf(iconP1), image); */
 }
 
-function stepHit(curStep:Int){
-    switch(curStep){
+function stepHit(curStep:Int) {
+    switch(curStep) {
         case 768:
-            FlxTween.tween(image , {x: -100}, 2, {ease: FlxEase.expoOut});
-            new FlxTimer().start(3, function(tmr:FlxTimer)
-                {
-                    FlxTween.tween(image , {x: 1700}, 3, {ease: FlxEase.backInOut});
-                });
+            /* FlxTween.tween(image , {x: -100}, 2, {ease: FlxEase.expoOut});
+            new FlxTimer().start(3, function(tmr:FlxTimer) {
+                FlxTween.tween(image , {x: 1700}, 3, {ease: FlxEase.backInOut});
+            }); */
     }
 }
