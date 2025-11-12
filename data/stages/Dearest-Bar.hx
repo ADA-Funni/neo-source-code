d22fe2b1fba28fb0c9e049f15a936a46ca1b76ae
💜 Rodney, An Imaginative Furball 💙 <okmilkrb@gmail.com>
2025-11-12 13:44:41 -0500

    Updated Dearest Bar, in college!
    * Made the crowd and lights only appear on high-memory.
    * Made crowd tint magenta on blacklight.

--- data/stages/Dearest-Bar.hx
+++ data/stages/Dearest-Bar.hx
@@ -1,1 +1,21 @@
-function postCreate() Crowd.visible = SONG.meta.displayName.toLowerCase() == "dadbattle";
+import utils.ColorHelp;
+
+// I SWEAR TO GOT I WISH I COULD PUT THESE TWO FUNCTIONS BELOW IN A SONG SCRIPT
+function onStageNodeParsed(event):Void {
+	if (PlayState.variation != 'blacklight') return;
+	var hasBlacklightTint:Bool = false;
+	if (event.node.attributeMap.exists('blacklightTint'))
+		hasBlacklightTint = event.node.attributeMap.get('blacklightTint') == 'true';
+	if (event.sprite.extra != null)
+		event.sprite.extra.set('blacklightTint', hasBlacklightTint);
+}
+function postCreate():Void {
+	if (PlayState.variation == 'blacklight')
+		for (element in stage.stageSprites)
+			if (element.extra.get('blacklightTint')) {
+				var colorTint = new ColorHelp(FlxColor.WHITE);
+				colorTint.magenta = 0.15;
+				element.color = colorTint.color;
+			}
+	Crowd.visible = SONG.meta.name == "dadbattle";
+}
