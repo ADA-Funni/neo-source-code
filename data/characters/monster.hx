d01500f067fd65a290738ee45b84f13500a75417
💜 Rodney, An Imaginative Furball 💙 <okmilkrb@gmail.com>
2025-11-12 18:11:37 -0500

    Updated Monsters heathdrain.
    * Now works in opponent play.
    * Drain cap to now effected by the option value.

--- data/characters/monster.hx
+++ data/characters/monster.hx
@@ -1,4 +1,12 @@
 function onNoteHit(event) {
-	if (event.characters.contains(this) && event.note.strumLine.cpu && PlayState.instance.health > 0.2)
-		event.healthGain = 0.025 * FlxG.save.data.healthdrain;
+	// works with opponent play ig
+	if (PlayState.instance != null && event.note.strumLine.cpu && event.characters.contains(this)) {
+		var healthBar:Float = PlayState.instance.healthBar;
+		var healthPercent:Float = healthBar.percent;
+		if (!event.note.strumLine.opponentSide)
+			healthPercent = (healthPercent - 100) * -1;
+
+		if (healthPercent > 10 * FlxG.save.data.healthdrain)
+			event.healthGain = 0.025 * FlxG.save.data.healthdrain;
+	}
 }
