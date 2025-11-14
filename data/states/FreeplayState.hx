60a12793c2a787af67f5b23b56e943aa975ae855
💜 Rodney, An Imaginative Furball 💙 <okmilkrb@gmail.com>
2025-11-14 16:59:45 -0500

    added perfect badge

--- data/states/FreeplayState.hx
+++ data/states/FreeplayState.hx
@@ -1,6 +1,39 @@
-function postUpdate(elapsed) {
-    for (i => spr in grpSongs.members) {
-        spr.x = lerp(spr.x, FlxG.width * 0.05 + (i * -25) + (curSelected == i ? 120 : 0), 0.05);
-        iconArray[i].x = spr.width + spr.x;
-    }
+import funkin.backend.utils.XMLUtil;
+import funkin.savedata.FunkinSave;
+
+var badgeSprite:FunkinSprite;
+function postCreate():Void {
+	badgeSprite = new FunkinSprite(FlxG.width - 280, -30);
+	badgeSprite.frames = Paths.getFrames('menus/freeplay/perfect_badge');
+	XMLUtil.addAnimToSprite(badgeSprite, {
+		name: 'idle',
+		anim: 'Perfect badge',
+		fps: 24,
+		loop: true
+	});
+	badgeSprite.scale.set(0.3, 0.3);
+	badgeSprite.playAnim('idle', true);
+	badgeSprite.scrollFactor.set();
+	add(badgeSprite);
 }
+
+function postUpdate(elapsed:Float):Void {
+	for (i => spr in grpSongs.members) {
+		spr.x = lerp(spr.x, FlxG.width * 0.05 + (i * -25) + (curSelected == i ? 120 : 0), 0.05);
+		iconArray[i].x = spr.width + spr.x;
+	}
+}
+
+function getSongScoreData(name:String, difficulty:String, ?variation:String, ?opponentMode:Bool, ?coopMode:Bool):{score:Int, accuracy:Float, misses:Int, hits:Map<String, Int>, date:String} {
+	var changes = [];
+	if (opponentMode ?? false) changes.push(Type.resolveEnum('funkin.savedata.HighscoreChange').COpponentMode);
+	if (coopMode ?? false) changes.push(Type.resolveEnum('funkin.savedata.HighscoreChange').CCoopMode);
+	return FunkinSave.getSongHighscore(name, difficulty, variation, changes);
+}
+function checkBadgeVisibility():Void {
+	var score = getSongScoreData(curSong.name, curDifficulties[curDifficulty], curSong.variant, __opponentMode, __coopMode);
+	badgeSprite.visible = score.accuracy == 100;
+}
+
+function onUpdateOptionsAlpha(event):Void
+	checkBadgeVisibility();
