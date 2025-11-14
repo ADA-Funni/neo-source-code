7b79250b6097fea421de7f3d0027d2053ae0a587
💜 Rodney, An Imaginative Furball 💙 <okmilkrb@gmail.com>
2025-11-14 18:36:10 -0500

    illusion transition

--- songs/south/scripts/yee-ha.hx
+++ songs/south/scripts/yee-ha.hx
@@ -7,3 +7,17 @@ function funnyBops(backItUp):Void {
 	camZoomingEvery = backup ? BeatType.Step : BeatType.Beat;
 	camZoomingInterval = backup ? 3 : 1;
 }
+
+var canEnd:Bool = !PlayState.isStoryMode;
+function onSongEnd(event):Void {
+	if (!canEnd)
+		event.cancel();
+	else return;
+
+	camGame.visible = camHUD.visible = false;
+	var sound = FlxG.sound.play(Paths.sound('Lights_Shut_off'), 0.7);
+	sound.onComplete = () -> {
+		canEnd = true;
+		endSong();
+	}
+}
