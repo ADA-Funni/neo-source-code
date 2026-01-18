4ca3b56dff8fee3dd2a41d374101727b1e078392
💜 Rodney, An Imaginative Furball 💙 <okmilkrb@gmail.com>
2026-01-18 18:47:54 -0500

    implemented tutorial voicelines

--- songs/tutorial/dialogue.hx
+++ songs/tutorial/dialogue.hx
@@ -2,3 +2,18 @@ function pauseMusic():Void
 	curMusic?.pause();
 function resumeMusic():Void
 	curMusic?.resume();
+
+var lineInt:Int = 0;
+var dialogueLine:FlxSound;
+function next(event):Void {
+	lineInt++;
+	trace(lineInt);
+	if (dialogueLine != null) dialogueLine.stop();
+	if (event.cancelled || !canProceed) return;
+
+	var soundPath = Paths.sound('dialogue-lines/tutorial/' + lineInt);
+	if (Assets.exists(soundPath)) {
+		dialogueLine = FlxG.sound.play(soundPath);
+		dialogueLine.autoDestroy = false;
+	}
+}
