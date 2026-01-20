25f0318c4c309df51e028dc47dce2b4675e7b6ac
💜 Rodney, An Imaginative Furball 💙 <okmilkrb@gmail.com>
2026-01-19 20:25:19 -0500

    implemented illusion voicelines

---
+++ songs/illusion/dialogue.hx
@@ -0,0 +1,21 @@
+function update(elapsed:Float):Void
+	if (controls.ACCEPT && !dialogueBox.dialogueEnded && dialogueLine != null)
+		dialogueLine.stop();
+
+var lineInt:Int = 0;
+var dialogueLine:FlxSound;
+function next(event):Void {
+	if (event.cancelled || !canProceed) return;
+	lineInt++; trace(lineInt);
+
+	var soundPath = Paths.sound('voicelines/illusion/' + lineInt);
+	if (Assets.exists(soundPath)) {
+		dialogueLine = FlxG.sound.play(soundPath);
+		dialogueLine.autoDestroy = false;
+	}
+}
+
+function close(event):Void {
+	if (event.cancelled) return;
+	if (dialogueLine != null) dialogueLine.destroy();
+}
