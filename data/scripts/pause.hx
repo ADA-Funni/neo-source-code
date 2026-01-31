ebc400e99f96cc4745eda479d5614b1e926f075f
💜 Rodney, An Imaginative Furball 💙 <okmilkrb@gmail.com>
2026-01-31 16:30:52 -0500

    Update pause.hx

--- data/scripts/pause.hx
+++ data/scripts/pause.hx
@@ -3,23 +3,24 @@
 var PARSE_ERROR:String = '[Parse Error --- Field Empty]';
 
 function getCharterText():String {
-	var general = PlayState.SONG?.meta?.customValues?.charter;
+	var general = PlayState.SONG?.meta?.customValues?.charter ?? PARSE_ERROR;
 	var difficulty = Reflect.getProperty(PlayState.SONG?.meta?.customValues, 'charter_' + PlayState.difficulty);
-	if (difficulty != null) return 'Charter (' + PlayState.difficulty + '): ' + difficulty;
-	return 'Charter: ' + (general ?? PARSE_ERROR);
+	return 'Charter: ' + (difficulty ?? general);
 }
 
 function postCreate():Void {
 	var composerText = new FunkinText(20, 15, 0, 'Composer: ' + (PlayState.SONG?.meta?.customValues?.composer ?? PARSE_ERROR), 32, false);
+	if (composerText.text == PARSE_ERROR) composerText.visible = false;
 	var charterText = new FunkinText(20, 15, 0, getCharterText(), 32, false);
+	if (charterText.text == PARSE_ERROR) charterText.visible = false;
 	for(k => label in [composerText, charterText]) {
 		if (label == null) continue;
+		add(label); if (!label.visible) continue;
 		label.scrollFactor.set();
 		label.alpha = 0;
 		label.x = FlxG.width - (label.width + 20);
 		label.y = 15 + (32 * (k + 4.3));
 		FlxTween.tween(label, {alpha: 1, y: label.y + 5}, 0.4, {ease: FlxEase.quartInOut, startDelay: 0.3 * (k + 1)});
-		add(label);
 	}
 	deathCounter.text = 'Deaths: ' + PlayState.deathCounter;
 	deathCounter.x = FlxG.width - (deathCounter.width + 20);
